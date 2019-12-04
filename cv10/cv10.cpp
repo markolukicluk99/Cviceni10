6fc338945d15ed63d34317c6b232270383a08841
Marko Lukic <markolukicluk99@gmail.com>
2019-12-04 20:09:30 +0100

    Konec - Nejdelsi slovo a jeho delka

--- cv10/cv10.cpp
+++ cv10/cv10.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "tchar.h"
 #include <ctype.h>
+#include <string.h>
 
 FILE* s;
 char retazec[200];
@@ -9,12 +10,13 @@ char output[50] = "Pismen je:";
 char* point = retazec;
 
 int pismena = 0;
+int i, pissmena, nejdelsi = 0, nejdelsipozice = 0;
 int cisla = 0;
 int sz = 1;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	fopen_s(&s, "vstup.txt", "r+");
+			/*   Otevreni textoveho souboru    */	fopen_s(&s, "vstup.txt", "r+");
 	printf("%p\n", s);
 	if (s == NULL) {
 		fopen_s(&s, "vstup.txt", "w+");
@@ -25,6 +27,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	sz = ftell(s);
 	fseek(s, 0L, SEEK_SET);
 
+	
+	
+	/*   Urceni pocet cisel ve vete, a pocet cislic    */
 	for (*point; *point != 0 && sz != 0; *point++) {
 		if (isalpha(*point)) {
 			pismena++;
@@ -33,14 +38,44 @@ int _tmain(int argc, _TCHAR* argv[])
 			cisla++;
 		}
 	}
+
+
+
+	/*   Nejdelsi slovo a jeho delka    */
+	for (i = 0; retazec[i] != '\0'; i++) {
+		for (pissmena = 0; retazec[i] != '\0' && retazec[i] != ' '; i++) {
+			pissmena++;
+		}
+		if (pissmena > nejdelsi) {
+			nejdelsi = pissmena;
+			nejdelsipozice = i - nejdelsi;
+		}
+	}
+
+	
+	
+	/****** TISK *******/
+
+	printf("Nejdelsi slovo: %d pismen, '%.*s'\n", nejdelsi, nejdelsi, retazec + nejdelsipozice);
+
 	printf("Pismen je:%d a cisel:%d\n", pismena, cisla);
 
 	fclose(s);
 
 
+
+	/* Zapis vysledku do souboru vystup.txt a jeho tisk */
+
 	fopen_s(&s, "vystup.txt", "w+");
+
 	fprintf(s, "Pismen je:%d a cisel:%d\n", pismena, cisla);
+
 	fclose(s);
 
 	return 0;
+
+
+	/*Marko Lukic  ID: 221282, Skupina: BPC-TLI03, Cviceni: Streda 17h*/
+
+
 }
